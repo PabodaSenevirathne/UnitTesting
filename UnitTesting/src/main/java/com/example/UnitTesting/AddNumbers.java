6506135e7bef920c9af4847c3710d7e948602a08
Paboda Senevirathne <pabodasenevirathne@gmail.com>
2020-11-17 17:50:13 +0530

    Add files via upload

---
+++ UnitTesting/src/main/java/com/example/UnitTesting/AddNumbers.java
@@ -0,0 +1,19 @@
+package com.example.UnitTesting;
+
+public class AddNumbers {
+    public long add(long x, long y) {
+        return x + y;
+    }
+
+    public long subtract(long x, long y) {
+        return x - y;
+    }
+
+    public long multiply(long x, long y) {
+        return x * y;
+    }
+
+    public long divide(long x, long y) {
+        return x / y;
+    }
+}
